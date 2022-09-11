2c5990795c9153da15b1d3584c7fc934eb176626
Ignacio Buioli <ibuioli@gmail.com>
2022-09-11 18:59:20 -0300

    feat: add msg for product not found

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -10,14 +10,17 @@ class OdooWebsite(http.Controller):
     @http.route(['/barcode_scanner', '/barcode_scanner/<int:barcode>'], auth="public", website=True)
     def barcode_scanner(self, barcode=False):
         if not barcode:
-            return request.render('website_barcode_scanner.barcode_scanner', {'product': False, 'combination': False})
+            return request.render('website_barcode_scanner.barcode_scanner', {'product': False, 'combination': False, 'notP': False})
         else:
             prod = http.request.env['product.template'].sudo().search([('barcode', '=', barcode)], limit=1)
-            pricelist = http.request.env['product.pricelist'].sudo().search([('id', '=', 1)], limit=1)
-            combination = prod._get_first_possible_combination()
-            combination_info = prod._get_combination_info(combination=combination, only_template=True, add_qty=1, pricelist=pricelist)
 
-            return request.render('website_barcode_scanner.barcode_scanner', {'product': prod, 'combination': combination_info, 'pricelist': pricelist})
+            if prod:
+                pricelist = http.request.env['product.pricelist'].sudo().search([('id', '=', 1)], limit=1)
+                combination = prod._get_first_possible_combination()
+                combination_info = prod._get_combination_info(combination=combination, only_template=True, add_qty=1, pricelist=pricelist)
+                return request.render('website_barcode_scanner.barcode_scanner', {'product': prod, 'combination': combination_info, 'pricelist': pricelist, 'notP': False})
+            else:
+                return request.render('website_barcode_scanner.barcode_scanner', {'product': False, 'notP': True})
 
     @http.route(['/get_product_barcode'], type='http', auth="public", website=True)
     def get_related_products(self, **kw):
